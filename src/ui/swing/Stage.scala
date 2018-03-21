131846ce8a902825b456bbc62e1e9bdd1a9c2e44
hessam-shm <hessam@ymail.com>
2018-03-21 11:33:52 +0330

    Rotation and some basic refactoring

--- src/ui/swing/Stage.scala
+++ src/ui/swing/Stage.scala
@@ -9,15 +9,43 @@ class Stage(size: (Int,Int)) {
   private[this] var blocks = Block((0,0),TKind) +: currentPiece.current
   def view: GameView = GameView(blocks,size,currentPiece.current)
 
-  def moveLeft() = moveBy(-1.0,0.0)
-  def moveRight() = moveBy(1.0,0.0)
-  private[this] def moveBy(delta: (Double,Double)): this.type = {
-    val unloaded = unload(currentPiece,blocks)
-    val moved = currentPiece.moveBy(delta)
-    blocks = load(moved,unloaded)
-    currentPiece = moved
+  def moveLeft() = transformPiece(_.moveBy(-1.0,0.0))
+  def moveRight() = transformPiece(_.moveBy(1.0,0.0))
+  def rotateCW() = transformPiece(_.rotateBy(-math.Pi/2.0))
+
+  /*private[this] def moveBy(delta: (Double,Double)): this.type = {
+    validate(currentPiece.moveBy(delta),
+      unload(currentPiece, blocks)) map { case (moved, unloaded) => blocks = load(moved, unloaded)
+      currentPiece = moved
+    }
     this
   }
+
+  private[this] def rotateBy(theta: Double): this.type = {
+    validate(currentPiece.rotateBy(theta),
+      unload(currentPiece,blocks)) map {case (moved,unloaded) =>
+        blocks = load(moved,unloaded)
+        currentPiece = moved
+    }
+    this
+  }*///TODO:test refactored part
+
+  private def transformPiece(trans: Piece => Piece): this.type = {
+    validate(trans(currentPiece),
+      unload(currentPiece,blocks)) map {case (moved,unloaded) =>
+        blocks = load(moved,unloaded)
+        currentPiece = moved
+    }
+    this
+  }
+
+  private[this] def validate(p: Piece, bs: Seq[Block]): Option[(Piece,Seq[Block])] =
+    if (p.current map {_.pos} forall inBounds) Some(p,bs)
+    else None
+
+  private[this] def inBounds(pos: (Int,Int)): Boolean =
+    (pos._1 >= 0) && (pos._1 < size._1) && (pos._2 >= 0) && (pos._2 < size._2)
+
   private[this] def unload(p: Piece, bs: Seq[Block]): Seq[Block] = {
     val currentPoss = p.current map {_.pos}
     bs filterNot {currentPoss contains _.pos}
